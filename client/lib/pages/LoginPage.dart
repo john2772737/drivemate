ac3f06d47e19da3392591ce59f4bf23c11f550a6
Neil <Jinxjinxx06@gmail.com>
2024-12-18 05:50:33 +0800

    Login Login UI

--- client/lib/pages/LoginPage.dart
+++ client/lib/pages/LoginPage.dart
@@ -1,209 +1,174 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import '../repository/auth.dart';
-import 'package:fluttertoast/fluttertoast.dart'; // Import the toast package
-import '../repository/user_service.dart'; // Import your UserService
+import 'package:fluttertoast/fluttertoast.dart';
+import '../repository/user_service.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    double screenWidth = MediaQuery.of(context).size.width;
     final AuthService _authService = AuthService();
-    final UserService _userService = UserService(); // Initialize UserService
+    final UserService _userService = UserService();
 
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 120),
-                child: Text(
-                  "Sign In",
-                  style: const TextStyle(fontSize: 30),
+      body: SafeArea(
+        child: Stack(
+          alignment: Alignment.center, // Center all elements horizontally
+          children: [
+            // Positioned "Sign In" Text at the Top
+            Positioned(
+              top: 80,
+              left: 135,
+              child: Text(
+                "Sign In",
+                style: const TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: "Nasalization"
                 ),
               ),
-              const SizedBox(height: 20),
-              Image.asset(
+            ),
+
+            // Positioned Car Image
+            Positioned(
+              top: 120, // Adjust top position for alignment
+              child: Image.asset(
                 'assets/images/dodge_signin.png',
-                height: 250,
+                height: 200,
+                fit: BoxFit.contain,
               ),
-              const SizedBox(height: 10),
-              Center(
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: const Color(0xFF282931),
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  height: 370,
-                  width: 300,
-                  child: Column(
-                    children: [
-                      const Padding(
-                        padding: EdgeInsets.only(top: 15.0),
-                        child: Text(
-                          'Step unto the gas!',
-                          style: TextStyle(color: Colors.white, fontSize: 18),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 15),
-                        child: Column(
-                          children: [
-                            const SizedBox(height: 15),
-                            TextField(
-                              decoration: InputDecoration(
-                                filled: true,
-                                fillColor: const Color(0xFFF4F6FF),
-                                border: const OutlineInputBorder(),
-                                hintText: 'Username',
-                                hintStyle: const TextStyle(fontSize: 12),
-                              ),
-                            ),
-                            const SizedBox(height: 15),
-                            TextField(
-                              obscureText: true,
-                              decoration: InputDecoration(
-                                hintText: 'Password',
-                                filled: true,
-                                fillColor: const Color(0xFFF4F6FF),
-                                border: const OutlineInputBorder(),
-                                hintStyle: const TextStyle(fontSize: 12),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      TextButton(
-                        onPressed: () {
-                          // Add Forgot Password Logic
-                        },
-                        child: const Text(
-                          'Forgot Password?',
-                          style: TextStyle(fontSize: 12, color: Colors.white),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 10),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            ElevatedButton(
-                              onPressed: () {
-                                // Add Explore button logic
-                              },
-                              style: ButtonStyle(
-                                backgroundColor:
-                                    MaterialStateProperty.all(Colors.black),
-                              ),
-                              child: const Text("Explore!"),
-                            ),
-                            ElevatedButton(
-                              onPressed: () {
-                                // Add "Let's Go!" logic
-                              },
-                              style: ElevatedButton.styleFrom(
-                                padding: const EdgeInsets.symmetric(
-                                    vertical: 12, horizontal: 20),
-                              ),
-                              child: Row(
-                                children: [
-                                  const Text("Let's Go!"),
-                                  const SizedBox(width: 10),
-                                  Image.asset(
-                                    'assets/images/customized_arrow.png',
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      const DividerSection(),
-                      ElevatedButton(
-                        onPressed: () async {
-                          final user = await _authService.signInWithGoogle();
-                          if (user != null) {
-                            print("Signed in as: ${user.displayName}");
+            ),
 
-                            var userData = {
-                              'email': user.email ?? '',
-                              'uid': user.uid ?? '',
-                            };
+            // Container Positioned to Align with Image
+            Positioned(
+              top: 250, // Adjust overlap with image
+              child: Container(
+                height: 350,
+                width: 300,
+                decoration: BoxDecoration(
+                  color: const Color(0xFF282931),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const SizedBox(height: 0),
+                    const Text(
+                      'Take the wheel!',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500,
+                        fontFamily: "Nasalization"
+                      ),
+                    ),
+                    const SizedBox(height: 15),
 
-                            // Save user data to MongoDB only if user doesn't already exist
-                            await _userService.saveUserToDatabase(userData);
-
-                            Fluttertoast.showToast(
-                              msg: "Google Sign-In Successful.",
-                              toastLength: Toast.LENGTH_LONG,
-                              gravity: ToastGravity.BOTTOM,
-                              backgroundColor: Colors.green,
-                              textColor: Colors.white,
-                              fontSize: 16.0,
-                            );
-                            context.go('/home'); // Navigate to home screen
-                          } else {
-                            // Show Toast message for failed login
-                            Fluttertoast.showToast(
-                              msg: "Google Sign-In failed.",
-                              toastLength: Toast.LENGTH_LONG,
-                              gravity: ToastGravity.BOTTOM,
-                              backgroundColor: Colors.red,
-                              textColor: Colors.white,
-                              fontSize: 16.0,
-                            );
-                          }
-                        },
-                        style: ButtonStyle(
-                          backgroundColor: MaterialStateProperty.all(
-                              const Color(0xFF282931)),
-                          elevation: MaterialStateProperty.all(0),
-                          padding: MaterialStateProperty.all(EdgeInsets.zero),
+                    // Stack to overlay Google button on the handle image
+                    Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        // Handle Image
+                        Image.asset(
+                          'assets/images/handle.png',
+                          height: 200,
                         ),
-                        child: Image.asset(
-                          'assets/images/google_icon.png',
-                          width: 32,
+                        // Google Sign-In Button centered on the handle image
+                        Positioned(
+                          top: -3,
+                          bottom: 10, // Adjust the vertical position of the button
+                          child: ElevatedButton(
+                            onPressed: () async {
+                              final user = await _authService.signInWithGoogle();
+                              if (user != null) {
+                                print("Signed in as: ${user.displayName}");
+
+                                Fluttertoast.showToast(
+                                  msg: "Google Sign-In Successful.",
+                                  toastLength: Toast.LENGTH_LONG,
+                                  gravity: ToastGravity.BOTTOM,
+                                  backgroundColor: Colors.green,
+                                  textColor: Colors.white,
+                                  fontSize: 16.0,
+                                );
+                                context.go('/home'); // Navigate to home screen
+                              } else {
+                                // Show Toast message for failed login
+                                Fluttertoast.showToast(
+                                  msg: "Google Sign-In failed.",
+                                  toastLength: Toast.LENGTH_LONG,
+                                  gravity: ToastGravity.BOTTOM,
+                                  backgroundColor: Colors.red,
+                                  textColor: Colors.white,
+                                  fontSize: 16.0,
+                                );
+                              }
+                            },
+                            style: ButtonStyle(
+                            backgroundColor: WidgetStateProperty.all(Colors.white),
+                            elevation: WidgetStateProperty.all(0),
+                            padding: WidgetStateProperty.all(
+                              const EdgeInsets.all(0),
+                            ),
+                            shape: WidgetStateProperty.all(
+                              const CircleBorder(),
+                            ),
+                            minimumSize: WidgetStateProperty.all(Size(30, 30)), // Set width and height here
+                          ),
+
+                            child: Image.asset(
+                              'assets/images/googleblack.png',
+                              height: 30,
+                              width: 30,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 10),
+
+                    // "Let's Go!" Button
+                    ElevatedButton(
+                      onPressed: () {
+                        // Add "Let's Go!" logic
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 12,
+                          horizontal: 30,
+                        ),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(100),
                         ),
                       ),
-                    ],
-                  ),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          const Text(
+                            "Let's Go!",
+                            style: TextStyle(
+                              color: Color(0xFF282931),
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(width: 10),
+                          Image.asset(
+                            'assets/images/customized_arrow.png',
+                            height: 20,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
-    );
-  }
-}
-
-class DividerSection extends StatelessWidget {
-  const DividerSection({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        const Expanded(
-          child: Divider(color: Colors.white, thickness: 1),
-        ),
-        const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 5),
-          child: Text(
-            "or continue with",
-            style: TextStyle(color: Colors.white),
-          ),
-        ),
-        const Expanded(
-          child: Divider(color: Colors.white, thickness: 1),
-        ),
-      ],
     );
   }
 }
